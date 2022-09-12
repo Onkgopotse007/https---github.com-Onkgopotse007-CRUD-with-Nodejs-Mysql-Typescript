a9c0289c2b876bcde5be3a189461b01acc04902b
Onkgopotse007 <72313429+Onkgopotse007@users.noreply.github.com>
2022-09-12 11:36:38 +0200

    Add files via upload

---
+++ people.sql
@@ -0,0 +1,57 @@
+use testdb;
+create table people (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50)
+);
+insert into people (id, first_name, last_name, email) values (1, 'Nicki', 'McDiarmid', 'nmcdiarmid0@mapquest.com');
+insert into people (id, first_name, last_name, email) values (2, 'Roberto', 'Danes', 'rdanes1@sakura.ne.jp');
+insert into people (id, first_name, last_name, email) values (3, 'Lannie', 'Parkman', 'lparkman2@google.pl');
+insert into people (id, first_name, last_name, email) values (4, 'Rosalinde', 'Harbach', 'rharbach3@illinois.edu');
+insert into people (id, first_name, last_name, email) values (5, 'Bellanca', 'Aumerle', 'baumerle4@google.ru');
+insert into people (id, first_name, last_name, email) values (6, 'Thelma', 'Gaynor', 'tgaynor5@virginia.edu');
+insert into people (id, first_name, last_name, email) values (7, 'Isahella', 'Dhenin', 'idhenin6@hugedomains.com');
+insert into people (id, first_name, last_name, email) values (8, 'Raddy', 'Glanert', 'rglanert7@webeden.co.uk');
+insert into people (id, first_name, last_name, email) values (9, 'Willi', 'Stanion', 'wstanion8@cnbc.com');
+insert into people (id, first_name, last_name, email) values (10, 'Tarah', 'Playfoot', 'tplayfoot9@cdbaby.com');
+insert into people (id, first_name, last_name, email) values (11, 'Leonora', 'Chasmer', 'lchasmera@europa.eu');
+insert into people (id, first_name, last_name, email) values (12, 'Clarisse', 'Hedworth', 'chedworthb@ustream.tv');
+insert into people (id, first_name, last_name, email) values (13, 'Alena', 'McNern', 'amcnernc@archive.org');
+insert into people (id, first_name, last_name, email) values (14, 'Phillipp', 'Manners', 'pmannersd@diigo.com');
+insert into people (id, first_name, last_name, email) values (15, 'Davina', 'Woodrow', 'dwoodrowe@i2i.jp');
+insert into people (id, first_name, last_name, email) values (16, 'Krishnah', 'Cowoppe', 'kcowoppef@ucoz.ru');
+insert into people (id, first_name, last_name, email) values (17, 'Abagail', 'Gatman', 'agatmang@ebay.com');
+insert into people (id, first_name, last_name, email) values (18, 'Shoshana', 'Lauchlan', 'slauchlanh@psu.edu');
+insert into people (id, first_name, last_name, email) values (19, 'Vilma', 'Slatten', 'vslatteni@ted.com');
+insert into people (id, first_name, last_name, email) values (20, 'Judas', 'Truscott', 'jtruscottj@acquirethisname.com');
+insert into people (id, first_name, last_name, email) values (21, 'Hedvig', 'Gilluley', 'hgilluleyk@nature.com');
+insert into people (id, first_name, last_name, email) values (22, 'Joseito', 'Auckland', 'jaucklandl@icq.com');
+insert into people (id, first_name, last_name, email) values (23, 'Skylar', 'Botright', 'sbotrightm@about.com');
+insert into people (id, first_name, last_name, email) values (24, 'Robby', 'Schutter', 'rschuttern@youtu.be');
+insert into people (id, first_name, last_name, email) values (25, 'Tyler', 'Ruddock', 'truddocko@umn.edu');
+insert into people (id, first_name, last_name, email) values (26, 'Hatti', 'Martinie', 'hmartiniep@washington.edu');
+insert into people (id, first_name, last_name, email) values (27, 'Gaston', 'Gellately', 'ggellatelyq@furl.net');
+insert into people (id, first_name, last_name, email) values (28, 'Cherise', 'Vaggs', 'cvaggsr@gravatar.com');
+insert into people (id, first_name, last_name, email) values (29, 'Joelle', 'Erricker', 'jerrickers@ovh.net');
+insert into people (id, first_name, last_name, email) values (30, 'Dniren', 'Riggulsford', 'driggulsfordt@twitter.com');
+insert into people (id, first_name, last_name, email) values (31, 'Ruthanne', 'Challis', 'rchallisu@accuweather.com');
+insert into people (id, first_name, last_name, email) values (32, 'Goldarina', 'Ilyinykh', 'gilyinykhv@opera.com');
+insert into people (id, first_name, last_name, email) values (33, 'Zaneta', 'Legonidec', 'zlegonidecw@ucoz.com');
+insert into people (id, first_name, last_name, email) values (34, 'Maynord', 'Toopin', 'mtoopinx@wp.com');
+insert into people (id, first_name, last_name, email) values (35, 'Shina', 'Ferrea', 'sferreay@wp.com');
+insert into people (id, first_name, last_name, email) values (36, 'Winne', 'Thow', 'wthowz@bandcamp.com');
+insert into people (id, first_name, last_name, email) values (37, 'Munroe', 'Izakoff', 'mizakoff10@moonfruit.com');
+insert into people (id, first_name, last_name, email) values (38, 'Vivian', 'Yetman', 'vyetman11@bravesites.com');
+insert into people (id, first_name, last_name, email) values (39, 'Jannelle', 'MacAnelley', 'jmacanelley12@ucla.edu');
+insert into people (id, first_name, last_name, email) values (40, 'Mar', 'Rawles', 'mrawles13@wikimedia.org');
+insert into people (id, first_name, last_name, email) values (41, 'Terrance', 'Louth', 'tlouth14@taobao.com');
+insert into people (id, first_name, last_name, email) values (42, 'Padriac', 'Rand', 'prand15@nymag.com');
+insert into people (id, first_name, last_name, email) values (43, 'Lynde', 'Knipe', 'lknipe16@stanford.edu');
+insert into people (id, first_name, last_name, email) values (44, 'Cristionna', 'Friedlos', 'cfriedlos17@vkontakte.ru');
+insert into people (id, first_name, last_name, email) values (45, 'Nananne', 'Rodrig', 'nrodrig18@prweb.com');
+insert into people (id, first_name, last_name, email) values (46, 'Harri', 'Olsson', 'holsson19@shutterfly.com');
+insert into people (id, first_name, last_name, email) values (47, 'Susy', 'Creasey', 'screasey1a@cam.ac.uk');
+insert into people (id, first_name, last_name, email) values (48, 'Ephrayim', 'Comelli', 'ecomelli1b@vk.com');
+insert into people (id, first_name, last_name, email) values (49, 'Hildagarde', 'Rosin', 'hrosin1c@cnn.com');
+insert into people (id, first_name, last_name, email) values (50, 'Melitta', 'Conman', 'mconman1d@de.vu');
